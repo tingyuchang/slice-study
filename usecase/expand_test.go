a9b72aa036001aea9a0e2e56ad587631d0abb7b0
tingyuchang <tingyugenius@gmail.com>
2021-08-05 11:38:38 +0800

    update benchmark

--- usecase/expand_test.go
+++ usecase/expand_test.go
@@ -2,13 +2,15 @@ package usecase
 
 import "testing"
 
-func BenchmarkInsert(b *testing.B) {
+func BenchmarkInsertByAppend(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		Insert(xi, 5, 99,98,97,96,95,94,93,92)
 	}
 }
 
-func BenchmarkInsert2(b *testing.B) {
+func BenchmarkInsertByCopy(b *testing.B) {
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		Insert2(xi, 5, 99,98,97,96,95,94,93,92)
 	}
